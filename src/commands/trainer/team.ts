42003b6fb79572e9983e3f4f576bf70b49261010
Cody Spratford <koreanpanda345@gmail.com>
2025-03-10 15:52:15 -0700

    caping the name in team command

--- src/commands/trainer/team.ts
+++ src/commands/trainer/team.ts
@@ -5,6 +5,7 @@ import {
 } from "discord.js";
 import BaseCommand from "../../base/BaseCommand.ts";
 import Databases from "../../databases/index.ts";
+import { Dex } from "@pkmn/dex";
 
 export default class TeamCommand extends BaseCommand {
   constructor() {
@@ -37,8 +38,9 @@ export default class TeamCommand extends BaseCommand {
 
     let pos = 1;
     for (const poke of pokemon) {
+      const dex = Dex.species.get(poke.species);
       embed.addFields({
-        name: `Level ${poke.level} ${poke.species}`,
+        name: `Level ${poke.level} ${dex.name}`,
         value: `Position: ${pos}`,
       });
       pos += 1;
